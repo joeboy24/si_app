661cfe3bb9dfc2566379ecdb7b6c753cd27a4bfe
Joseph Oduro-Gyan <joeboy24.jb@gmail.com>
2024-07-16 12:09:47 -0400

    Initial Commit

---
+++ si_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  si_app
+//
+//  Created by Joseph Oduro-Gyan on 7/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ si_app/si_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  si_appApp.swift
+//  si_app
+//
+//  Created by Joseph Oduro-Gyan on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct si_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
